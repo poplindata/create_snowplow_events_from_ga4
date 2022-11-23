1f1eb71990f43271558f8c20734616cd385137c6
zloff <zoe@poplindata.com>
2022-11-23 16:14:36 +1100

    sql update

--- query_bq.py
+++ query_bq.py
@@ -6,11 +6,10 @@ app.config["DEBUG"] = True #If the code is malformed, there will be an error sho
 
 project_id = 'snowflake-snowplow-217500'
 dataset = 'analytics_341844832'
-table = 'events_'
+table = 'events_*'
 
 
 sql = f"""
-    CREATE OR REPLACE TABLE ga4_test.ga4_events AS (
     SELECT 
     TIMESTAMP_MICROS(event_Timestamp) as event_timestamp,
     event_name,
@@ -25,12 +24,15 @@ sql = f"""
     end) as ga_session_id,
     FROM `{project_id}.{dataset}.{table}`, unnest(event_params) as event_params
     group by event_timestamp, event_name
-    )
 """
 
 @app.route('/translate_ga4_to_snowplow', methods=['GET'])
 def ga4_to_sp_events():
     df = pandas_gbq.read_gbq(sql, project_id=project_id)
-    five = 5
-    return f"Queried table! {five},{df.head()}"
+    pandas_gbq.to_gbq(dataframe=df, destination_table='ga4_test.ga4_events', project_id=project_id)
+    return f"""
+        Queried table row count:\n 
+        {df.shape[0]}
+        New table has been created
+    """
     
